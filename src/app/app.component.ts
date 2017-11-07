3063905105eda6125da6df4d89dd07e87ef4062d
Yun Kui Pan <panyunkui@gmail.com>
2017-11-07 12:53:22 -0500

    added some info about the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Welcome to weka-api web!';
+  description = "It's an ongoing personal project. Main idea is to build a web (front+back) to provide user-friendly weka prediction models. See how far I can go. So much fun!!!";
   architecture = {
     // _here_to_add_
     'backend': ['Java Spring boot','Weka'], 
-    'frontend':['Angular 4', 'TypeScript']
+    'frontend':['Angular 4', 'TypeScript'],
+    'deployment':['Heroku','GitHub']
   }
 }
