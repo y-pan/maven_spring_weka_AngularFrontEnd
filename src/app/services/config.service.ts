c45b6a654c9d11674b3c2e14f90351d12dc849e5
Yun Kui Pan <panyunkui@gmail.com>
2017-12-11 11:58:14 -0500

    use apigee proxy pointing to heroku url

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 
 enum SERVER_OPTIOIN{
-  HEROKU, LOCAL
+  HEROKU, LOCAL, APIGEE
 }
 
 @Injectable()
 
 export class ConfigService {
 
-  private serverOption:SERVER_OPTIOIN = SERVER_OPTIOIN.HEROKU;    // here decise what kind of server to use, default use heroku
+  private serverOption:SERVER_OPTIOIN = SERVER_OPTIOIN.APIGEE;    // here decise what kind of server to use, default use heroku
   private localServerPort: string = "8080";
   private selectedModel:string = null;
 
@@ -16,8 +16,8 @@ export class ConfigService {
   getServerRootUrl():string{
     switch(this.serverOption){
       case SERVER_OPTIOIN.LOCAL: return "http://localhost:"+this.localServerPort+"";
-
-      case SERVER_OPTIOIN.HEROKU: 
+      case SERVER_OPTIOIN.HEROKU: return "https://maven-spring-weka.herokuapp.com";
+      case SERVER_OPTIOIN.APIGEE: return "http://panyunkui2-eval-test.apigee.net/webweka";
       default :return "https://maven-spring-weka.herokuapp.com";
     }
   }
